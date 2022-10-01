b3754c5a9c73f57b009a0e0aaed2fd2ecfb561c1
Vittcenko <evgenav83@gmail.com>
2022-10-02 00:14:15 +0300

    added Slider

---
+++ JS/10_Slick_Slider/src/js/app.js
@@ -0,0 +1,84 @@
+$(function() {
+  $('.slider_block').slick({
+      dots: true, 
+      infinite: true,
+      prevArrow: false,
+      nextArrow: false
+  });
+})
+
+
+$(function() {
+    $('.slider_products').slick({
+        slidesToShow: 5,
+        nextArrow: '<button type="button" class="slick-prev slick_arrow"><img src="./img/prev.svg"/></button>',
+        prevArrow: '<button type="button" class="slick-next slick_arrow"><img src="./img/prev.svg"/></button>',       
+        responsive: [
+            {
+              breakpoint: 1024, //екран менше 1024
+              settings: {
+                slidesToShow: 5,
+                arrows: true,                
+                // infinite: true, по замовчуванню
+                // dots: true  точки поки не треба
+               }
+            },
+            {
+                breakpoint: 768, //екран менше 768
+                settings: {
+                  slidesToShow: 3,
+                  arrows: true
+                  // infinite: true, по замовчуванню
+                  // dots: true  точки поки не треба
+               }
+            },
+            {
+                breakpoint: 568, //екран менше 568
+                settings: {
+                  slidesToShow: 1,
+                  // infinite: true, по замовчуванню
+                  dots: true, 
+                  arrows: false
+                }
+            }
+        ]    
+    });
+})
+
+$(function() {
+    $('.slider_partners').slick({
+        slidesToShow: 9,
+        nextArrow: '<button type="button" class="slick-prev slick_arrow"><img src="./img/prev.svg"/></button>',
+        prevArrow: '<button type="button" class="slick-next slick_arrow"><img src="./img/prev.svg"/></button>',
+        responsive: [
+            {
+              breakpoint: 1024, //екран менше 1024
+              settings: {
+                slidesToShow: 9,
+                arrows: true
+                // infinite: true, по замовчуванню
+                // dots: true  точки поки не треба
+              }
+            },
+            {
+                breakpoint: 768, //екран менше 768
+                settings: {
+                  slidesToShow: 6,
+                  arrows: true
+                  // infinite: true, по замовчуванню
+                  // dots: true  точки поки не треба
+                }
+            },
+            {
+                breakpoint: 568, //екран менше 568
+                settings: {
+                  slidesToShow: 2,
+                  // infinite: true, по замовчуванню
+                  dots: true, 
+                  arrows: false
+                }
+            }
+        ]    
+    });
+})
+
---
+++ JS/10_Slick_Slider/src/js/dist/event.dev.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(function () {
+  $(".slider_card_item").click(function () {
+    $(".slider_card_item").removeClass('active');
+    $(this).addClass('active');
+  });
+}); // function changeStatus() {
+//     const div = document.querySelector('.slider_card_item')
+//     div.addEventListener('click', e => {
+//         console.log(e.target);
+//     })
+// }
